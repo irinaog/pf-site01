d0754cb25a380a71fd51400878f40a65d90df668
irinaog <goncharirina08@gmail.com>
2022-12-08 15:15:27 +0300

    compiler scss

--- gulpfile.js
+++ gulpfile.js
@@ -18,14 +18,16 @@ import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
 import { server } from './gulp/tasks/server.js';
+import { scss } from './gulp/tasks/scss.js';
 
 //наблюдатель за изменениями в файлах
 function watcher() {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss)
 }
 
-const mainTasks = gulp.parallel(copy, html);
+const mainTasks = gulp.parallel(copy, html, scss);
 
 //построение сценарией выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher,server));
